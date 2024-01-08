1f90ad60093a4a30dc10b476a97410ba23c6b8b7
NickRayman <nlhrayman@gmail.com>
2024-01-08 15:18:45 -0500

    added js functionality

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const artists=getElementById("artists");
 document.querySelector(artists).append(h2);
 //h2.insertBefore(getElementById("images"));
 h2.id="javas";*/
+
+const pressMe = document.getElementById("press");
+const colors = ["red", "purple", "green", "pink", "blue", "rgb(7,192,254)"]
+
+var num=-1;
+pressMe.addEventListener('click', function() {
+    if(num==5 || num==-1){
+        num=-1;
+        
+    }
+    console.log(num);
+    document.body.style.backgroundColor=colors[num+=1];
+}
+  );
+  
