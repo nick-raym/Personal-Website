1d2227bb59d1b94c08179312deaefb9d73bb1056
NickRayman <nlhrayman@gmail.com>
2024-01-24 23:49:55 -0500

    red/green comparisons mma and formatting

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/*const h2 = document.createElement("h2");
-h2.textContent = "Some of my Favorite Artists";
-const artists=getElementById("artists");
-document.querySelector(artists).append(h2);
-//h2.insertBefore(getElementById("images"));
-h2.id="javas";*/
+
 kanyeQuote()
 const pressMe = document.getElementById("press");
 const colors = ["lightcoral", "purple", "green", "pink", "blue", "rgb(7,192,254)"]
@@ -14,7 +9,6 @@ pressMe.addEventListener('click', function() {
         num=-1;
         
     }
-    console.log(num);
     document.body.style.backgroundColor=colors[num+=1];
 }
   );
@@ -27,7 +21,6 @@ pressMe.addEventListener('click', function() {
         yeQuote.textContent = '"'+quote.quote+'"'
         yeQuote.id = "kanye-quote"
         document.querySelector("#quote").appendChild(yeQuote)
-        console.log(yeQuote.textContent)
         return yeQuote.textContent
     });
   }
@@ -43,33 +36,15 @@ pressMe.addEventListener('click', function() {
 
   })
 
-  // function mmaStats(){
-  //   fetch("https://api.sportradar.us/mma/trial/v2/en/champions.json?api_key=hg9s33jqdr37es6y8vz5qd4j")
-  //   .then(response=>response.json())
-  //   .then(mmaInfo=>{
-  //     console.log(mmaInfo)
-  //   })
-  //   .catch(error=>console.log(error))
-  // }
-  
-  // mmaStats()
-
-
-  //add submit to kanye quote to add a comment
-  //on hover do something
-  
   function zipFindLocation(){
     const form = document.querySelector("#find-zip")
     form.addEventListener("submit",event=>{
       event.preventDefault()
         const zipCode = form.enterZip.value;
         const country = form.enterCountry.value;
-        console.log(zipCode +" "+country)
         fetch("https://api.zippopotam.us/"+country+"/"+zipCode)
         .then(response=>response.json())
         .then(zipLo=>{
-          //console.log(("https://api.zippopotam.us/"+country+"/"+zipCode))
-          //console.log(zipLo)
           const zipList = document.querySelector("#zipList")
           const zipInfo = "Zip Location: " +zipLo["post code"]+", "+zipLo["country"]+" "+"Place Name: " + zipLo["places"][0]["place name"] +", "+zipLo["places"][0]["state abbreviation"]+" "+"Lat/Long: "+zipLo["places"][0]["latitude"]+", "+zipLo["places"][0]["longitude"]
           const zipEl = document.createElement("p")
@@ -100,7 +75,6 @@ pressMe.addEventListener('click', function() {
     return per 
   });
 
-  console.log(periods)
 
   document.querySelector("#hover").addEventListener("mouseover",event=>{
     event.preventDefault()
@@ -122,6 +96,8 @@ pressMe.addEventListener('click', function() {
   .then(fighters=>{
     const mmaFormOne = document.querySelector("#mma-first")
     const mmaFormTwo = document.querySelector("#mma-second")
+    let fighterOneName="";
+    let fighterTwoName="";
 
     mmaFormOne.addEventListener("submit",event=>{
       event.preventDefault()
@@ -132,7 +108,7 @@ pressMe.addEventListener('click', function() {
         name = firstLetter + name.substring(1)
         return name
       })
-      const fighterOneName = capArray.join(" ")
+      fighterOneName = capArray.join(" ")
       const firstStats= document.querySelector("#first-stats")
     
       fighters.forEach(fighter => {
@@ -142,7 +118,9 @@ pressMe.addEventListener('click', function() {
             firstStats.append(newStat)
           }
       })
+      compareStats(fighterOneName,fighterTwoName,fighters)
     })
+    
 
     mmaFormTwo.addEventListener("submit",event=>{
       event.preventDefault()
@@ -153,7 +131,7 @@ pressMe.addEventListener('click', function() {
         name = firstLetter + name.substring(1)
         return name
       })
-      const fighterTwoName = capArray.join(" ")
+      fighterTwoName = capArray.join(" ")
       const secondStats= document.querySelector("#second-stats")
     
       fighters.forEach(fighter => {
@@ -163,8 +141,9 @@ pressMe.addEventListener('click', function() {
             secondStats.append(newStat)
           }
       })
+      compareStats(fighterOneName,fighterTwoName,fighters)
     })
-
+    
   
   })
   .catch(error=>{
@@ -177,13 +156,162 @@ pressMe.addEventListener('click', function() {
       if(!(fighter["nickname"]==="")){
         newStat.innerHTML='<em>"'+fighter["nickname"]+'"</em>'+'<br>'
         }
-        newStat.innerHTML+="Record: "+"("+fighter["wins"]+", "+fighter["losses"]+", "+fighter["draws"]+")"
-        newStat.innerHTML+="<br>Ht: "+fighter["height_cm"]+"cm Wt: "+fighter["weight_in_kg"]+"kg Reach: "+fighter["reach_in_cm"]+"cm"
+        newStat.innerHTML+="Record: "+"(<span class='wins'>"+fighter["wins"]+"</span>, <span class='losses'>"+fighter["losses"]+"</span>, <span class='draws'>"+fighter["draws"]+"</span>)"
+        newStat.innerHTML+="<br>Ht: <span class='height'>"+fighter["height_cm"]+"cm</span> Wt: <span class='weight'>"+fighter["weight_in_kg"]+"kg</span> Reach: <span class='reach'>"+fighter["reach_in_cm"]+"cm</span>"
         newStat.innerHTML+="<br>"+fighter["stance"]+" Stance"
-        newStat.innerHTML+="<br>SSLPM: "+fighter["significant_strikes_landed_per_minute"]+" SS Accuracy: "+fighter["significant_striking_accuracy"]+"%"
-        newStat.innerHTML+="<br>SS-absorbed-Pm: "+fighter["significant_strikes_absorbed_per_minute"]+" SS-Def: "+fighter["significant_strike_defence"]+"%"
-        newStat.innerHTML+="<br>TDLPM: "+fighter["average_takedowns_landed_per_15_minutes"]+" TDACC: "+fighter["takedown_accuracy"]+"% TD-Def: "+fighter["takedown_defense"]+"%"
-        newStat.innerHTML+="<br> Avg-Subs-Att/15mins: "+fighter["average_submissions_attempted_per_15_minutes"]
+        newStat.innerHTML+="<br>SSLPM: <span class='sslpm'>"+fighter["significant_strikes_landed_per_minute"]+"</span> SS Accuracy: <span class='ssa'>"+fighter["significant_striking_accuracy"]+"%</span>"
+        newStat.innerHTML+="<br>SS-absorbed-Pm: <span class='ssapm'>"+fighter["significant_strikes_absorbed_per_minute"]+"</span> SS-Def: <span class='ssd'>"+fighter["significant_strike_defence"]+"%</span>"
+        newStat.innerHTML+="<br>TDLPM: <span class='atl'>"+fighter["average_takedowns_landed_per_15_minutes"]+"</span> TDACC: <span class='tda'>"+fighter["takedown_accuracy"]+"%</span> TD-Def: <span class='tdd'>"+fighter["takedown_defense"]+"%</span>"
+        newStat.innerHTML+="<br> Avg-Subs-Att/15mins: <span class='sub'>"+fighter["average_submissions_attempted_per_15_minutes"]+"</span>"
         return newStat
 
+  }
+
+
+  function compareStats(fighter1,fighter2,fighters){
+    if(fighter1!="" && fighter2!=""){
+      let winsOne,winsTwo,lossesOne,lossesTwo,drawsOne,drawsTwo,heightOne,heightTwo,weightOne,weightTwo,reachOne,reachTwo,sslpmOne,sslpmTwo,ssaOne,ssaTwo,ssapmOne,ssapmTwo,ssdOne,ssdTwo,atlOne,atlTwo,tdaOne,tdaTwo,tddOne,tddTwo,subOne,subTwo;
+      fighters.forEach(fighter => {
+        if(fighter1 === fighter["name"]){
+           winsOne=fighter["wins"]
+           lossesOne=fighter["losses"]
+           drawsOne = fighter["draws"]
+           heightOne=fighter["height_cm"]
+           weightOne=fighter["weight_in_kg"]
+           reachOne=fighter["reach_in_cm"]
+           sslpmOne=fighter["significant_strikes_landed_per_minute"]
+           ssaOne=fighter["significant_striking_accuracy"]
+           ssapmOne=fighter["significant_strikes_absorbed_per_minute"]
+           ssdOne=fighter["significant_strike_defence"]
+           atlOne=fighter["average_takedowns_landed_per_15_minutes"]
+           tdaOne=fighter["takedown_accuracy"]
+           tddOne=fighter["takedown_defense"]
+           subOne=fighter["average_submissions_attempted_per_15_minutes"]
+
+        }
+        if(fighter2 === fighter["name"]){
+           winsTwo=fighter["wins"]
+           lossesTwo=fighter["losses"]
+           drawsTwo = fighter["draws"]
+           heightTwo=fighter["height_cm"]
+           weightTwo=fighter["weight_in_kg"]
+           reachTwo=fighter["reach_in_cm"]
+           sslpmTwo=fighter["significant_strikes_landed_per_minute"]
+           ssaTwo=fighter["significant_striking_accuracy"]
+           ssapmTwo=fighter["significant_strikes_absorbed_per_minute"]
+           ssdTwo=fighter["significant_strike_defence"]
+           atlTwo=fighter["average_takedowns_landed_per_15_minutes"]
+           tdaTwo=fighter["takedown_accuracy"]
+           tddTwo=fighter["takedown_defense"]
+           subTwo=fighter["average_submissions_attempted_per_15_minutes"]
+
+        }
+      
+        })
+
+        if(winsOne>winsTwo){
+          document.querySelector("#first-stats .wins").style.color="green"
+          document.querySelector("#second-stats .wins").style.color="red"
+        }
+        else if(winsOne<winsTwo){
+          document.querySelector("#first-stats .wins").style.color="red"
+          document.querySelector("#second-stats .wins").style.color="green"
+        }
+        if(lossesOne<lossesTwo){
+          document.querySelector("#first-stats .losses").style.color="green"
+          document.querySelector("#second-stats .losses").style.color="red"
+        }
+        else if(lossesOne>lossesTwo){
+          document.querySelector("#first-stats .losses").style.color="red"
+          document.querySelector("#second-stats .losses").style.color="green"
+        }
+        if(heightOne>heightTwo){
+          document.querySelector("#first-stats .height").style.color="green"
+          document.querySelector("#second-stats .height").style.color="red"
+        }
+        else if(heightOne<heightTwo){
+          document.querySelector("#first-stats .height").style.color="red"
+          document.querySelector("#second-stats .height").style.color="green"
+        }
+        if(weightOne>weightTwo){
+          document.querySelector("#first-stats .weight").style.color="green"
+          document.querySelector("#second-stats .weight").style.color="red"
+        }
+        else if(weightOne<weightTwo){
+          document.querySelector("#first-stats .weight").style.color="red"
+          document.querySelector("#second-stats .weight").style.color="green"
+        }
+        if(reachOne>reachTwo){
+          document.querySelector("#first-stats .reach").style.color="green"
+          document.querySelector("#second-stats .reach").style.color="red"
+        }
+        else if(reachOne<reachTwo){
+          document.querySelector("#first-stats .reach").style.color="red"
+          document.querySelector("#second-stats .reach").style.color="green"
+        }
+        if(sslpmOne>sslpmTwo){
+          document.querySelector("#first-stats .sslpm").style.color="green"
+          document.querySelector("#second-stats .sslpm").style.color="red"
+        }
+        else if(sslpmOne<sslpmTwo){
+          document.querySelector("#first-stats .sslpm").style.color="red"
+          document.querySelector("#second-stats .sslpm").style.color="green"
+        }
+        if(ssaOne>ssaTwo){
+          document.querySelector("#first-stats .ssa").style.color="green"
+          document.querySelector("#second-stats .ssa").style.color="red"
+        }
+        else if(ssaOne<ssaTwo){
+          document.querySelector("#first-stats .ssa").style.color="red"
+          document.querySelector("#second-stats .ssa").style.color="green"
+        }
+        if(ssapmOne<ssapmTwo){
+          document.querySelector("#first-stats .ssapm").style.color="green"
+          document.querySelector("#second-stats .ssapm").style.color="red"
+        }
+        else if(ssapmOne>ssapmTwo){
+          document.querySelector("#first-stats .ssapm").style.color="red"
+          document.querySelector("#second-stats .ssapm").style.color="green"
+        }
+        if(ssdOne>ssdTwo){
+          document.querySelector("#first-stats .ssd").style.color="green"
+          document.querySelector("#second-stats .ssd").style.color="red"
+        }
+        else if(ssdOne<ssdTwo){
+          document.querySelector("#first-stats .ssd").style.color="red"
+          document.querySelector("#second-stats .ssd").style.color="green"
+        }
+        if(atlOne>atlTwo){
+          document.querySelector("#first-stats .atl").style.color="green"
+          document.querySelector("#second-stats .atl").style.color="red"
+        }
+        else if(atlOne<atlTwo){
+          document.querySelector("#first-stats .atl").style.color="red"
+          document.querySelector("#second-stats .atl").style.color="green"
+        }
+        if(tdaOne>tdaTwo){
+          document.querySelector("#first-stats .tda").style.color="green"
+          document.querySelector("#second-stats .tda").style.color="red"
+        }
+        else if(tdaOne<tdaTwo){
+          document.querySelector("#first-stats .tda").style.color="red"
+          document.querySelector("#second-stats .tda").style.color="green"
+        }
+        if(tddOne>tddTwo){
+          document.querySelector("#first-stats .tdd").style.color="green"
+          document.querySelector("#second-stats .tdd").style.color="red"
+        }
+        else if(tddOne<tddTwo){
+          document.querySelector("#first-stats .tdd").style.color="red"
+          document.querySelector("#second-stats .tdd").style.color="green"
+        }
+        if(subOne>subTwo){
+          document.querySelector("#first-stats .sub").style.color="green"
+          document.querySelector("#second-stats .sub").style.color="red"
+        }
+        else if(subOne<subTwo){
+          document.querySelector("#first-stats .sub").style.color="red"
+          document.querySelector("#second-stats .sub").style.color="green"
+        }
+    }
   }
